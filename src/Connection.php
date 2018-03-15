f07ecd748de61991462dac5cbecb3c48c586ec4a
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-03-15 14:38:40 -0300

    Update Connection.php

--- src/Connection.php
+++ src/Connection.php
@@ -5,9 +5,36 @@ namespace Uspdev\Replicado;
 class Connection
 {
     public $db;
+    private $dbHost;
+    private $dbPort;
+    private $dbName;
+    private $dbUser;
+    private $dbPassword;
+    
     public function __construct($dbHost,$dbPort,$dbName,$dbUser,$dbPassword)
     {
-        $this->db = new \PDO("dblib:tdsver=5.0;host=$dbHost:$dbPort", $dbUser,$dbPassword);
-        $this->db->query("use $dbName");
+        $this->dbHost = $dbHost;
+        $this->dbPort = $dbPort;
+        $this->dbName = $dbName;
+        $this->dbUser = $dbUser;
+        $this->dbPassword = $dbPassword;
     }
+    
+    public function sybase($dbHost,$dbPort,$dbName,$dbUser,$dbPassword)
+    {
+        $this->db = new \PDO("dblib:tdsver=5.0;host=$this->dbHost:$this->dbPort", $this->dbUser,$this->dbPassword);
+        $this->db->query("use $this->dbName");
+        return $this->db;
+    }
+    
+    public function mssql($dbHost,$dbPort,$dbName,$dbUser,$dbPassword)
+    {
+        $dsn= "dblib:host=$this->dbHost:$this->dbPort;dbname=$this->dbName;";
+        $dbusername=$this->dbUser;
+        $dbpassword=$this->dbPassword;
+        $this->db = new \PDO($dsn,$dbusername,$dbpassword);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        return $this->db;
+    }
+    
 }
