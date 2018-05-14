8d05b00764ed656cd37ca8c88829f90fcbfb0d88
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2018-05-14 19:58:09 -0300

    busca por parte do Nome

--- src/Graduacao.php
+++ src/Graduacao.php
@@ -9,7 +9,9 @@ class Graduacao
         $cols = file_get_contents('replicado_queries/tables/localizapessoa.sql', true);
         $query = " SELECT {$cols} FROM LOCALIZAPESSOA WHERE codpes = '{$codpes}'"; 
         $result = DB::fetchAll($query);
+        //var_dump($result); die();
 
+        $return = false;
         foreach($result as $row)
         {
             if(trim($row['tipvin']) == 'ALUNOGR' && 
@@ -17,19 +19,23 @@ class Graduacao
                trim($row['codundclg']) == $codundclgi) 
                $return = true;
         }
-        return false;
+        return $return;
     }
 
-    # Exemplo:
-    # $strFiltro = "AND PESSOA.nompes LIKE '%Alessandro%'"
-    public static function ativos($codundclgi, $strFiltro = '')
+    public static function ativos($codundclgi, $parteNome = null)
     {
         $cols1 = file_get_contents('replicado_queries/tables/localizapessoa.sql', true);
         $cols2 = file_get_contents('replicado_queries/tables/pessoa.sql', true);
-        $query = " SELECT {$cols1},{$cols2} FROM LOCALIZAPESSOA "; 
-        $query .= " INNER JOIN PESSOA ON (LOCALIZAPESSOA.codpes = PESSOA.codpes) "; 
+        $query = " SELECT {$cols1},{$cols2},UPPER(PESSOA.nompes) as nompes_upper FROM LOCALIZAPESSOA ";
+
+        $query .= " INNER JOIN PESSOA ON (LOCALIZAPESSOA.codpes = PESSOA.codpes) ";
         $query .= " WHERE LOCALIZAPESSOA.tipvin = 'ALUNOGR' AND LOCALIZAPESSOA.codundclg = '{$codundclgi}' ";
-        $query .= " {$strFiltro} "; 
+
+        if (!is_null($parteNome)) {
+            $parteNome = trim(utf8_decode(Uteis::removeAcentos($parteNome)));
+            $parteNome = strtoupper(str_replace(' ','%',$parteNome));
+            $query .= " AND nompes_upper LIKE '%{$parteNome}%' ";
+        }
         $query .= " ORDER BY PESSOA.nompes ASC "; 
 
         $result = DB::fetchAll($query);
@@ -58,7 +64,7 @@ class Graduacao
         $query .= " WHERE (LOCALIZAPESSOA.codpes = $codpes) ";
         $query .= " AND (LOCALIZAPESSOA.tipvin = 'ALUNOGR' AND LOCALIZAPESSOA.codundclg = '{$codundclgi}') ";
         $query .= " AND (VINCULOPESSOAUSP.codcurgrd = HABILITACAOGR.codcur AND VINCULOPESSOAUSP.codhab = HABILITACAOGR.codhab) ";
-        $result = DB::fetchAll($query);
+        $result = DB::fetch($query);
         $result = Uteis::utf8_converter($result);
         $result = Uteis::trim_recursivo($result);
 
