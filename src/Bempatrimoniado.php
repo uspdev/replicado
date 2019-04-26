db21ec98f5c170da980b5d53daa171ecf8642eea
Thiago Gomes Verissimo <thiago.verissimo@usp.br>
2019-04-26 15:43:01 -0300

    Corrige classe patrimonio

--- src/Bempatrimoniado.php
+++ src/Bempatrimoniado.php
@@ -5,18 +5,22 @@ namespace Uspdev\Replicado;
 class Bempatrimoniado
 {
 
-    #private static informatica = [12513,51110,354384,354341,162213,9300,45624,57100];
+    private static $BEM_INFORMATICAS = [12513,51110,354384,354341,162213,9300,45624,57100];
 
     public static function dump($numpat)
     {
-        $query = " SELECT * FROM BEMPATRIMONIADO WHERE numpat = :numpat";
+        $query = " SELECT * FROM BEMPATRIMONIADO WHERE numpat = convert(decimal,:numpat)";
         $param = [
             'numpat' => $numpat,
         ];
+
         $result = DB::fetch($query, $param);
-        #$result = Uteis::utf8_converter($result);
-        #$result = Uteis::trim_recursivo($result);
-        return $result;
+        if (!empty($result)) {
+            $result = Uteis::utf8_converter($result);
+            $result = Uteis::trim_recursivo($result);
+            return $result;
+        }
+        return false;
     }
 
     public static function verifica($numpat)
@@ -40,23 +44,10 @@ class Bempatrimoniado
 
     public static function isInformatica($numpat)
     {
-        $result = $this->dump($numpat);
-        if (isset($result) && in_array($result['coditmmat'],$this->bems_informatica)) {
+        $result = Bempatrimoniado::dump($numpat);
+        if (isset($result) && in_array($result['coditmmat'],self::$BEM_INFORMATICAS)) {
             return true;
-        }
-     
+        } 
         return false;
-    }
-    public static function ativosInformatica(){
-    /*
-     WHERE (BEMPATRIMONIADO.coditmmat = '12513'  OR 
-            BEMPATRIMONIADO.coditmmat = '51110'  OR 
-            BEMPATRIMONIADO.coditmmat = '354384' OR 
-            BEMPATRIMONIADO.coditmmat = '354341' OR 
-            BEMPATRIMONIADO.coditmmat = '9300' OR 
-            BEMPATRIMONIADO.coditmmat = '162213' OR 
-            BEMPATRIMONIADO.coditmmat = '57100' OR 
-            BEMPATRIMONIADO.coditmmat = '45624')
-     */    
     }
 }
