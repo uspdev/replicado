15baa45bc1f80e9158e2b0017095f9b5ca732a3a
Lucas Flóro <lucas@fearp.usp.br>
2019-10-15 16:24:24 -0300

    Fix #171 Alteração/melhorias no método Bempatrimoniado->ativos
    Criado método bens($filtros = [], $buscas = [], $tipos = [])
    Alterado método ativos($filtros = [], $buscas = [], $tipos = [])

--- src/Bempatrimoniado.php
+++ src/Bempatrimoniado.php
@@ -34,40 +34,19 @@ class Bempatrimoniado
     }
 
     /**
-     * Retorna todos bens patrimoniados ativos (com opção de filtro)
+     * Retorna todos bens patrimoniados ativos (com opção de filtros e buscas)
      *  
-     * @param array $params (opcional) - campo_tabela => valor
-     * @param string $operador (default = AND) - operador da cláusula WHERE
+     * @param array $filtros (default) - stabem => 'Ativo'
+     * @param array $buscas (opcional) - campo_tabela => valor
+     * @param array $tipos (opcional) - campo_tabela => tipo (ex.: codpes => int)
      * 
      * @return array Retorna todos os campos da tabela BEMPATRIMONIADO
      */
-    public static function ativos($params = null, $operador = 'AND')
+    public static function ativos($filtros = [], $buscas = [], $tipos = [])
     {
-        if (is_null($params)) {
-            $query = " SELECT * FROM BEMPATRIMONIADO WHERE stabem = 'Ativo'";
-        } else {
-            $query = " SELECT * FROM BEMPATRIMONIADO WHERE stabem = 'Ativo' AND (";
-
-            foreach ($params as $campo => $valor) {
-                if (gettype($valor) == 'integer') {
-                    $query .= " {$campo} = {$valor} ";
-                } else if (gettype($valor) == 'string') {
-                    $valor = Uteis::removeAcentos($valor);
-                    $query .= " {$campo} LIKE '%{$valor}%' COLLATE Latin1_General_CI_AI ";
-                }
-
-                // Enquanto houver item no array, adiciona AND/OR na clásula WHERE
-                if (next($params)) {
-                    $query .= $operador;
-                } else {
-                    $query .= ")";
-                }
-            }
-        }
-
-        $result = DB::fetchAll($query);
-        $result = Uteis::utf8_converter($result);
-        $result = Uteis::trim_recursivo($result);
+        $filtros['stabem'] = 'Ativo';
+        $result = self::bens($filtros, $buscas, $tipos);
+        
         return $result;
     }
 
@@ -79,4 +58,74 @@ class Bempatrimoniado
         } 
         return false;
     }
+
+    /**
+     * Retorna todos bens patrimoniados (com opção de filtros e buscas)
+     *  
+     * @param array $filtros (opcional) - campo_tabela => valor
+     * @param array $buscas (opcional) - campo_tabela => valor
+     * @param array $tipos (opcional) - campo_tabela => tipo (ex.: codpes => int)
+     * 
+     * @return array Retorna todos os campos da tabela BEMPATRIMONIADO
+     */
+    public static function bens($filtros = [], $buscas = [], $tipos = [])
+    {
+        $query = " SELECT * FROM BEMPATRIMONIADO ";
+        $filtros_buscas = self::criaFiltroBusca($filtros, $buscas, $tipos);
+        // Atualiza a cláusula WHERE do sql
+        $query .= $filtros_buscas[0];
+        // Define os parâmetros para cláusula WHERE
+        $params = $filtros_buscas[1];
+    
+        $result = DB::fetchAll($query, $params);
+        $result = Uteis::utf8_converter($result);
+        $result = Uteis::trim_recursivo($result);
+        return $result;
+    }
+
+    public static function criaFiltroBusca($filtros, $buscas, $tipos)
+    {
+        // Abre o parênteses dos filtros
+        $str_where = " WHERE (";
+        $params = [];
+        if (!empty($filtros) && (count($filtros) > 0)) {
+            foreach ($filtros as $coluna => $valor) {
+                if (array_key_exists($coluna, $tipos)) {
+                    $str_where .= " {$coluna} = convert({$tipos[$coluna]}, :{$coluna}) ";
+                    $params[$coluna] = "{$valor}";
+                } else {
+                    $str_where .= " {$coluna} = :{$coluna} ";
+                    $params[$coluna] = "{$valor}";
+                }
+                // Enquanto existir um filtro, adiciona o operador AND
+                if (next($filtros)) {
+                    $str_where .= ' AND ';
+                }
+            }
+        }
+
+        if (!empty($buscas) && (count($buscas) > 0)) {
+            // Caso exista um campo para busca, fecha os parênteses anterior
+            // e adiciona mais um AND (, que conterá os parâmetros de busca (OR)
+            $str_where .= ') AND (';
+            foreach ($buscas as $coluna => $valor) {
+                $str_where .= " {$coluna} LIKE :{$coluna} ";
+                $params[$coluna] = "%{$valor}%";
+
+                // Enquanto existir uma busca, adiciona o operador OR
+                if (next($buscas)) {
+                    $str_where .= ' OR ';
+                } else {
+                    // Fecha o parênteses do OR
+                    $str_where .= ') ';
+                }
+            }
+        } else {
+            // Fecha o parênteses dos filtros
+            $str_where .= ')';
+        }
+
+        return [$str_where, $params];
+    }
 }
+
