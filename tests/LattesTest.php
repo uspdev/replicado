bc0eca6840966929fa3357bdbaa0c10bd769dc3e
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2020-10-15 13:52:33 -0300

    Test para Lattes ok

--- tests/LattesTest.php
+++ tests/LattesTest.php
@@ -3,9 +3,22 @@
 namespace Uspdev\Replicado\Tests;
 
 use PHPUnit\Framework\TestCase;
-use Uspdev\Replicado\Pessoa;
+use Uspdev\Replicado\Lattes;
+use Uspdev\Replicado\DB;
 
 class LattesTest extends TestCase
 {
+    public function test_idLattes(){
+        DB::getInstance()->prepare('DELETE FROM DIM_PESSOA_XMLUSP')->execute();
 
+        $sql = "INSERT INTO DIM_PESSOA_XMLUSP (codpes, idfpescpq) VALUES 
+                                   (convert(int,:codpes),convert(int,:idfpescpq))";
+        $data = [
+            'codpes' => 123456,
+            'idfpescpq' => 658585
+        ];
+        DB::getInstance()->prepare($sql)->execute($data);
+
+        $this->assertSame('658585',Lattes::idLattes(123456)['idLattes']);
+    }
 }
