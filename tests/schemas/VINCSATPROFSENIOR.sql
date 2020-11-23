e296236eaaafc47a1038024ffdbded90cc75f55e
Gabriela Reis <gabrielareisg@hotmail.com>
2020-11-23 15:11:06 -0300

    salvando mudanças

--- tests/schemas/VINCSATPROFSENIOR.sql
+++ tests/schemas/VINCSATPROFSENIOR.sql
@@ -1,5 +1,8 @@
 create table VINCSATPROFSENIOR (
     codpes int not null,
     codund tinyint not null,
-    dtafimcbd smalldatetime not null
+    dtafimcbd smalldatetime not null, 
+    numseqpes tinyint not null,
+    tipvin char(25) not null,
+    dtainicbd smalldatetime not null
 )
