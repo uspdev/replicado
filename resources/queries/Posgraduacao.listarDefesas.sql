6fd2aba49e3458290595c41a4f5b6a46a2aa7b6e
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2021-01-29 12:45:13 -0300

    trazendo mais colunas nas defesas

--- resources/queries/Posgraduacao.listarDefesas.sql
+++ resources/queries/Posgraduacao.listarDefesas.sql
@@ -3,6 +3,9 @@ t1.codpes,
 nompes = (SELECT nompes FROM PESSOA WHERE codpes=t1.codpes),
 t1.dtadfapgm, -- Data da Defesa
 t1.nivpgm,    -- ME/DO
+t3.codare,    -- Código da área
+t6.nomare,    -- Nome da área, na maiorias das vezes coincide com nomcur
+t4.codcur,    -- Nome do programa de Pós-Graduação
 t4.nomcur,    -- Nome do programa de Pós-Graduação
 t5.tittrb     -- Título da Dissertação / Tese
 
@@ -23,6 +26,11 @@ ON (
   t1.numseqpgm = t5.numseqpgm
   AND t1.codpes = t5.codpes
   AND t1.codare = t5.codare
+)
+
+INNER JOIN NOMEAREA t6
+ON (
+  t3.codare = t6.codare
 )
 
 -- Filtros
