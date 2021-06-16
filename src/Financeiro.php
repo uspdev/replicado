d446e0c1d26343f7237fecb23c241a10f6e893ce
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2021-06-16 16:17:40 -0300

    ajustes issue_449

--- src/Financeiro.php
+++ src/Financeiro.php
@@ -5,39 +5,21 @@ namespace Uspdev\Replicado;
 class Financeiro
 {
      /**
-     * Método que recebe o Código da Unidade e retorna os centros de despesas da mesma.
-     * Caso não seja passado a unidade, pega o REPLICADO_CODUNDCLG do .env
+     * Método que retorna os centros de despesas.
+     * Utiliza o REPLICADO_CODUNDCLG do .env
      * 
-     * @param Integer $codund - código da Unidade
+     * @param empty
      * @return array
      * @author Victor de O. Marinho <victor.oliveira.marinho@usp.br>
      */
-    public static function listarCentrosDespesas($codund = null)
+    public static function listarCentrosDespesas()
     {
+        $unidades = getenv('REPLICADO_CODUNDCLG');
         $query = "SELECT etrhie FROM CENTRODESPHIERARQUIA
-                    WHERE dtadtv IS NULL";
-        
-        if ($codund) {
-            $query .= " AND codunddsp = convert(int,:codund)";
-            $param = [
-                'codund' => $codund,
-            ];
-            $result = DB::fetchAll($query, $param);
-        }else{
-            $unidades = explode(",",getenv('REPLICADO_CODUNDCLG'));
-            $i = 0;
-            foreach($unidades as $unidade){
-                if($i == 0){
-                    $query .= " AND codunddsp = $unidade";
-                }
-                else{
-                    $query .= " OR codunddsp = $unidade";
-                }
-                $i++;
-            }
-            $result = DB::fetchAll($query);
-        }      
-        return $result;
+                    WHERE dtadtv IS NULL
+                    AND codunddsp IN ({$unidades})";
+                          
+        return DB::fetchAll($query);
     }
 
 }
