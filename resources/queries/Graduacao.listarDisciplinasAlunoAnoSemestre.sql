34e568be1dc55b0152e966cc17fea3de55240ec2
Masaki Kawabata Neto <kawabata@usp.br>
2021-12-10 09:49:15 -0300

    novo metodo listarDisciplinasAlunoAnoSemestre #482

---
+++ resources/queries/Graduacao.listarDisciplinasAlunoAnoSemestre.sql
@@ -0,0 +1,17 @@
+SELECT
+    DISTINCT PROF.nompes, PROF.codpes, O.codtur, T.tiptur, D.coddis, D.nomdis, D.verdis, H.rstfim
+FROM
+    ALUNOGR A INNER JOIN PROGRAMAGR PR ON A.codpes=PR.codpes
+    INNER JOIN HISTESCOLARGR H ON PR.codpes=H.codpes AND PR.codpgm=H.codpgm
+    INNER JOIN TURMAGR T ON H.coddis=T.coddis AND H.verdis=T.verdis AND H.codtur=T.codtur
+    INNER JOIN DISCIPLINAGR D ON T.coddis=D.coddis AND T.verdis=D.verdis
+    INNER JOIN OCUPTURMA O ON T.coddis=O.coddis AND T.verdis=O.verdis AND T.codtur=O.codtur
+    INNER JOIN MINISTRANTE M ON O.coddis=M.coddis AND O.codtur=M.codtur AND O.verdis=M.verdis
+    INNER JOIN PESSOA PROF ON M.codpes=PROF.codpes
+WHERE
+    A.codpes = :codpes AND
+    T.codtur LIKE CONCAT(:anoSemestre, '%') AND
+    H.stamtr = 'M'
+    __rstfim__
+ORDER BY
+    D.nomdis;
