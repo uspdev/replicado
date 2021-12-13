1ab8781edc5e1754c44dde67bc6d88cffbc36e08
Ricardo Fernando dos Santos <st.ricardof@gmail.com>
2021-12-13 12:53:40 -0300

    colegiados

--- resources/queries/Pessoa.listarTitularesDoColegiado.sql
+++ resources/queries/Pessoa.listarTitularesDoColegiado.sql
@@ -1,3 +1,55 @@
+
+SELECT
+  PARTICIPANTECOLEG.codpes as titular, 
+  PARTICIPANTECOLEG.tipfncclg,
+  PARTICIPANTECOLEG.dtainimdt,
+  PARTICIPANTECOLEG.dtafimmdt
+
+FROM PARTICIPANTECOLEG
+  INNER JOIN COLEGIADO ON COLEGIADO.codclg=PARTICIPANTECOLEG.codclg
+  -- Somente representações ativas
+WHERE PARTICIPANTECOLEG.dtafimmdt > :dtafimmdt
+
+-- Somente da unidade e colegiado em questão
+  AND COLEGIADO.codundrsp IN (__unidades__)
+  AND COLEGIADO.codclg = convert(int,:codclg)
+  AND PARTICIPANTECOLEG.tipfncclg in ('Presidente','Vice-Presidente')
+
+  UNION 
+SELECT
+  PARTICIPANTECOLEG.codpes as titular, 
+  PARTICIPANTECOLEG.tipfncclg,
+  PARTICIPANTECOLEG.dtainimdt,
+  PARTICIPANTECOLEG.dtafimmdt
+
+FROM PARTICIPANTECOLEG
+  INNER JOIN COLEGIADO ON COLEGIADO.codclg=PARTICIPANTECOLEG.codclg
+  -- Somente representações ativas
+WHERE PARTICIPANTECOLEG.dtafimmdt > :dtafimmdt
+
+-- Somente da unidade e colegiado em questão
+  AND COLEGIADO.codundrsp IN (__unidades__)
+  AND COLEGIADO.codclg = convert(int,:codclg)
+  AND PARTICIPANTECOLEG.tipfncclg != 'Suplente'
+  AND PARTICIPANTECOLEG.codpes NOT IN 
+    (
+    SELECT
+    PARTICIPANTECOLEG.codpes  
+  FROM PARTICIPANTECOLEG
+    INNER JOIN COLEGIADO ON COLEGIADO.codclg=PARTICIPANTECOLEG.codclg
+    -- Somente representações ativas
+  WHERE PARTICIPANTECOLEG.dtafimmdt > :dtafimmdt
+  -- Somente da unidade e colegiado em questão
+    AND COLEGIADO.codundrsp IN (__unidades__)
+    AND COLEGIADO.codclg = convert(int,:codclg)
+    AND PARTICIPANTECOLEG.tipfncclg in ('Presidente','Vice-Presidente')
+   
+    )
+  order by titular
+
+
+  /**
+
 SELECT
   PARTICIPANTECOLEG.codpes,
   PARTICIPANTECOLEG.tipfncclg,
@@ -14,3 +66,4 @@ WHERE PARTICIPANTECOLEG.dtafimmdt > :dtafimmdt
   AND COLEGIADO.codundrsp IN (__unidades__)
   AND COLEGIADO.codclg = convert(int,:codclg)
   AND PARTICIPANTECOLEG.tipfncclg != 'Suplente'
+  **/
--- resources/queries/Pessoa.retornarEmailUsp.sql
+++ resources/queries/Pessoa.retornarEmailUsp.sql
@@ -1,4 +1,4 @@
 SELECT DISTINCT codema 
 FROM EMAILPESSOA
 WHERE codpes = convert(int,:codpes)
-AND codema LIKE '%@usp.br%'
+AND codema LIKE '%usp.br%'
