4b0cd783a1696ab0a885f8ee0fcc106ec544cf34
Erickson Zanon <ezanon@gmail.com>
2023-06-14 11:38:32 -0300

    #545 pequenos ajustes

--- resources/queries/CEU.listarCursos.sql
+++ resources/queries/CEU.listarCursos.sql
@@ -1,26 +1,29 @@
-SELECT  e.codcurceu, e.codedicurceu, c.nomcurceu,  
-        c.codsetdep, s.nomset, 
-        cast(c.objcur as NVARCHAR(MAX)) as objcur ,cast(c.juscur as NVARCHAR(MAX)) as juscur,  
-        c.fmtcurceu, c.dscpbcinr, e.dtainiofeedi,  
-        e.dtafimofeedi, e.qtdvagofe, f.numpro, f.staedi, count(m.codpes) as matriculados 
+SELECT  
+    e.codcurceu, e.codedicurceu, 
+    c.nomcurceu, cast(c.objcur as NVARCHAR(MAX)) as objcur, cast(c.juscur as NVARCHAR(MAX)) as juscur, c.dscpbcinr, c.fmtcurceu, 
+    s.codset, s.nomset, s.nomabvset, 
+    ec.numpro, ec.staedi,
+    convert(varchar, e.dtainiofeedi, 103) as dtainiofeedi, convert(varchar, e.dtafimofeedi, 103) as dtafimofeedi, e.qtdvagofe, 
+    count(m.codpes) as matriculados 
 FROM EDICAOCURSOOFECEU e 
 LEFT JOIN CURSOCEU c ON c.codcurceu = e.codcurceu   
 LEFT JOIN SETOR s ON c.codsetdep = s.codset 
-LEFT JOIN EDICAOCURSOCEU f ON (f.codcurceu = c.codcurceu AND f.codedicurceu = e.codedicurceu) 
+LEFT JOIN EDICAOCURSOCEU ec ON (ec.codcurceu = c.codcurceu AND ec.codedicurceu = e.codedicurceu) 
 LEFT JOIN MATRICULACURSOCEU m ON (m.codcurceu = c.codcurceu AND m.codedicurceu = e.codedicurceu) 
 WHERE  
- c.codclg in (__codundclgs__) 
-__deptos__
+    c.codclg in (__codundclgs__) 
+    __deptos__
+    AND
+        ec.staedi != 'CAN' -- edição do curso cancelada
     AND  
-        (year(e.dtainiofeedi) BETWEEN convert(int,:ano_inicio) AND convert(int,:ano_fim) 
+        ((year(e.dtainiofeedi) BETWEEN convert(int,:ano_inicio) AND convert(int,:ano_fim)) 
         OR  
         (year(e.dtafimofeedi) BETWEEN convert(int,:ano_inicio) AND convert(int,:ano_fim))) 
-group by e.codcurceu, e.codedicurceu, c.nomcurceu,  
-        c.codsetdep, s.nomset,
-        cast(c.objcur as NVARCHAR(MAX)) ,cast(c.juscur as NVARCHAR(MAX)),  
-        c.fmtcurceu, c.dscpbcinr, e.dtainiofeedi,  
-        e.dtafimofeedi, e.qtdvagofe, f.numpro, f.staedi
+GROUP BY -- o group by com quase todos os itens do select é para funcionar o 'count(m.codpes) as matriculados'
+    e.codcurceu, e.codedicurceu, 
+    c.nomcurceu, cast(c.objcur as NVARCHAR(MAX)), cast(c.juscur as NVARCHAR(MAX)), c.dscpbcinr, c.fmtcurceu, 
+    s.codset, s.nomset, s.nomabvset, 
+    ec.numpro, ec.staedi,
+    e.dtainiofeedi, e.dtafimofeedi, e.qtdvagofe
 ORDER BY  
     e.dtainiofeedi
-
--- o group by com quase todos os itens do select é para funcionar o 'count(m.codpes) as matriculados'
--- src/CEU.php
+++ src/CEU.php
@@ -2,84 +2,65 @@
 
 namespace Uspdev\Replicado;
 
-class CEU
-{
-    
+class CEU {
+
     /**
      * Método para retornar os cursos de cultura e extensão de um período, permite filtrar por departamentos também 
      * @param int $ano_inicio - ano inicial do período (se null, usa o ano atual)
      * @param int $ano_fim - ano final do período (se null, usa igual $ano_inicio ano atual)
      * @param array $deptos - Recebe um array ou valores separados por vírgula com os códigos dos departamentos desejados. Se for igual a null, a consulta retornará todos os departamentos.
      * @return array
-     * @author Erickson Zanon <ezanon@gmail.com> alterado em 06/2023 https://github.com/uspdev/replicado/issues/545
-     */    
-    public static function listarCursos($ano_inicio = null, $ano_fim = null, $deptos = null){
-        
+     * @author Erickson Zanon <ezanon@gmail.com> alterado em 06/2023 #545
+     */
+    public static function listarCursos($ano_inicio = null, $ano_fim = null, $deptos = null) {
+
         // se não foi enviado $ano_inicio, atribui o ano atual
-        if (!$ano_inicio) $ano_inicio = date('Y');
+        $ano_inicio = $ano_inicio ?: date('Y');
         // se não foi enviado $ano_fim, atribui mesmo valor do $ano_inicio
-        if (!$ano_fim) $ano_fim = $ano_inicio;
-            
+        $ano_fim = $ano_fim ?: $ano_inicio;
+
         $query = DB::getQuery('CEU.listarCursos.sql');
-        
-        //adiciona as unidades
-//        $unidades = getenv('REPLICADO_CODUNDCLG');
-//        $query = str_replace('__unidades__', $unidades, $query);
-        
+
         // adiciona os departamentos   
-        $deptoo = '';
-        if ($deptos != NULL){ 
-            if (!is_array($deptos)){ // enviado valores separados por vírgula ou um único valor
-                $deptoo = "AND C.codsetdep IN ($deptos)";
+        $query_deptos = '';
+        if ($deptos != NULL) {
+            if (!is_array($deptos)) { // enviado valores separados por vírgula ou um único valor
+                $query_deptos = "AND C.codsetdep IN ($deptos)";
+            } else { // enviado array
+                $query_deptos = implode(",", $deptos);
+                $query_deptos = "AND C.codsetdep IN ($query_deptos)";
             }
-            else { // enviado array
-                $deptoo = implode(",", $deptos);
-                $deptoo = "AND C.codsetdep IN ($deptoo)";
-            }  
         }
-        $query = str_replace('__deptos__',$deptoo, $query);  
-        
+        $query = str_replace('__deptos__', $query_deptos, $query);
+
         // executando a query
         $param = [
             'ano_inicio' => $ano_inicio,
-            'ano_fim' => $ano_fim       
+            'ano_fim' => $ano_fim
         ];
-        $result_cursos = DB::fetchAll($query,$param);
-        
+        $result_cursos = DB::fetchAll($query, $param);
+
         // recuperar mais informações dos cursos obtidos
         $cursos = [];
-        foreach($result_cursos as $curso){
-            
-            // se a edição foi cancelada não continua pois não será devolvido este valor
-            if ($curso['staedi']=='CAN'){
-                unset($curso);
-                continue;
-            }
-            
+        foreach ($result_cursos as $curso) {
+
             // obter ministrantes
             $query = DB::getQuery('CEU.listarMinistrantesPorCurso.sql');
             $param = [
                 'codcurceu' => $curso['codcurceu'],
-                'codedicurceu' => $curso['codedicurceu']       
+                'codedicurceu' => $curso['codedicurceu']
             ];
             $result_ministrantes = DB::fetchAll($query, $param);
-            if (count($result_ministrantes) == 0){
-                $curso['ministrantes'] = '-';
+            $ministrantes = [];
+            foreach ($result_ministrantes as $m) {
+                array_push($ministrantes, $m['nompes']);
             }
-            else{
-                $ministrantes = [];
-                foreach ($result_ministrantes as $m){
-                    array_push($ministrantes, $m['nompes']);
-                }
-                $curso['ministrantes'] = implode(", ", $ministrantes);
-            }
-            
+            $curso['ministrantes'] = implode(", ", $ministrantes);
+
             // adiciona o curso ao array cursos que será retornado
             array_push($cursos, $curso);
-            unset($curso);
-
-        }       
+        }
         return $cursos;
     }
-    
+
 }
