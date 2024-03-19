47c7c05a565947d5be0bab4e77c7797bc8583d91
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2024-03-19 16:32:46 -0300

    Add Campus

--- resources/queries/Estrutura.listarUnidadesAtivas.sql
+++ resources/queries/Estrutura.listarUnidadesAtivas.sql
@@ -1,1 +1,4 @@
-SELECT * FROM UNIDADE WHERE dtadtvund IS NULL ORDER BY nomund
+SELECT U.*, C.* FROM UNIDADE U
+INNER JOIN CAMPUS C ON U.codcam = C.codcam  
+WHERE U.dtadtvund IS NULL AND C.stacam = 'O' 
+ORDER BY C.nomofccam, U.nomund
