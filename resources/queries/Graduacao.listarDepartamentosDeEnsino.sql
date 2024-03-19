62a9ce0de1ac6c1d92d272e28ff1b54b08f1f1bc
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2024-03-19 16:36:17 -0300

    Só setores ativos

--- resources/queries/Graduacao.listarDepartamentosDeEnsino.sql
+++ resources/queries/Graduacao.listarDepartamentosDeEnsino.sql
@@ -1,1 +1,1 @@
-SELECT * FROM SETOR  WHERE codund IN (__codundclg__) AND tipset = 'Departamento de Ensino' ORDER BY nomset
+SELECT * FROM SETOR  WHERE codund IN (__codundclg__) AND tipset = 'Departamento de Ensino'  AND dtadtvset IS NULL ORDER BY nomset
