d6caa085164e2a957f477da86e5efa1c014f7cd3
Kawan Santana <kawansantana18@gmail.com>
2024-03-20 09:10:45 -0300

    Usando coluna numpticam no join

--- resources/queries/Estrutura.listarUnidades.sql
+++ resources/queries/Estrutura.listarUnidades.sql
@@ -1,4 +1,4 @@
-SELECT U.*, C.* FROM UNIDADE U
-INNER JOIN CAMPUS C ON U.codcam = C.codcam  
-WHERE U.dtadtvund IS NULL AND C.stacam = 'O' 
+SELECT  C.*, U.* FROM UNIDADE U
+INNER JOIN CAMPUS C ON U.codcam = C.codcam AND C.numpticam = U.numpticam
+WHERE U.dtadtvund IS NULL 
 ORDER BY C.nomofccam, U.nomund
