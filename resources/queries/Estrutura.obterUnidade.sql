7bec3d083b0fd012bb4a915faf99edfc43eab9cc
Alessandro Costa de Oliveira <alecosta.web@gmail.com>
2024-06-11 11:31:37 -0300

    Add localidade

--- resources/queries/Estrutura.obterUnidade.sql
+++ resources/queries/Estrutura.obterUnidade.sql
@@ -1,4 +1,5 @@
-SELECT U.*, E.* 
-FROM UNIDADE U, ENDUSP E 
+SELECT U.*, E.*, L.*
+FROM UNIDADE U, ENDUSP E, LOCALIDADE L
 WHERE U.codund = CONVERT(int, :codund)
 AND (E.numseqendusp = 1 AND E.codund = U.codund)
+AND L.codloc = E.codloc
